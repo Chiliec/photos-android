fae1ae025dac3e887f439416864b5c5786a80137
Vladimir Babin <vovababin@gmail.com>
2018-05-02 17:36:13 +0500

    Add detail activity opening via Intent

---
+++ app/src/main/java/info/babin/photos/DetailActivity.kt
@@ -0,0 +1,25 @@
+package info.babin.photos
+
+import android.os.Bundle
+import android.app.Activity
+import android.widget.ImageView
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+
+class DetailActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        if (intent.hasExtra("photo_url")) {
+            val url = intent.getStringExtra("photo_url")
+            val image = findViewById<ImageView>(R.id.detailImageView)
+            GlideApp
+                .with(image)
+                .load(url)
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(image)
+        }
+    }
+
+}
--- app/src/main/java/info/babin/photos/PhotosAdapter.kt
+++ app/src/main/java/info/babin/photos/PhotosAdapter.kt
@@ -1,5 +1,6 @@
 package info.babin.photos
 
+import android.content.Intent
 import android.content.res.Resources
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ class PhotosAdapter(private val photos: Array<String>) : RecyclerView.Adapter<Ph
 
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val imageView = v.findViewById(R.id.imageView) as ImageView
+        var imageUrl = ""
     }
 
     override fun onCreateViewHolder(parent: ViewGroup,
@@ -22,11 +24,17 @@ class PhotosAdapter(private val photos: Array<String>) : RecyclerView.Adapter<Ph
         val halfDisplaySize = Resources.getSystem().displayMetrics.widthPixels / 2
         viewHolder.imageView.layoutParams.height = halfDisplaySize
         viewHolder.imageView.layoutParams.width = halfDisplaySize
+        viewHolder.imageView.setOnClickListener({ _ ->
+            val intent = Intent(parent.context, DetailActivity::class.java)
+            intent.putExtra("photo_url", viewHolder.imageUrl)
+            parent.context.startActivity(intent)
+        })
         return viewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val url = photos[position]
+        holder.imageUrl = url
         GlideApp
             .with(holder.imageView)
             .load(url)
